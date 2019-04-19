06823c256f1299900e08650812382db00377265a
e <1144709265@qq.com>
2019-04-19 19:59:33 +0800

    3

---
+++ 12.正式课第十二周/ketang/src1/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.less"
+ReactDOM.render(
+    <div className="app">
+        <div className="aa">12345</div>
+    </div>,
+document.querySelector("#root")
+)
+;
